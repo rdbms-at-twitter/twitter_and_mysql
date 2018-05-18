f6f61715a55171d0c0124613a2b1b038c00cdbb1
rdbms-at-twitter <shinya.s.jpn@gmail.com>
2018-05-18 13:02:53 +0900

    Add CRRENT_TIMESTAMP to the table

--- T_Tweets.sql
+++ T_Tweets.sql
@@ -2,6 +2,7 @@ CREATE TABLE `T_Tweets` (
   `tweet_id` bigint(20) NOT NULL,
   `user` varchar(512) DEFAULT NULL,
   `text` varchar(4096) DEFAULT NULL,
+  `tweet_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`tweet_id`),
   FULLTEXT KEY `idx_mecab_text` (`text`) /*!50100 WITH PARSER `mecab` */ 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
